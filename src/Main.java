60ebd02bde2cfd0a17ada8257f4eba3d8bc4a361
Name Surname <vladislavabasarina8@gmail.com>
2023-11-25 06:44:10 +0300

    change commit

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,17 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int isnachalna_summa = 100;
-        int oplata = 1100;
-        int itogo = isnachalna_summa + oplata;
+        int initialAmount = 100;
+        int payment = 1100;
+        int inTotal = initialAmount + payment;
 
-        if (oplata < 1000) {
-            System.out.println("Итого =" + itogo);
+        if (payment < 1000) {
+            System.out.println("Итого =" + inTotal);
         } else {
-            int itogo1 = itogo + oplata / 100;
-            System.out.println("Итого =" + itogo1);
+            int inTotalAmount = inTotal + payment / 100;
+            System.out.println("Итого =" + inTotalAmount);
         }
 
 
-    }}
+    }
+}
